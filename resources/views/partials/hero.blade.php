4a0186799395b1d38767f5bd08b4edf2a32e5573
igor shim <igor.shimanskyi@gmail.com>
2021-07-20 16:48:17 +0300

    add dots & lines, add responsive for header & hero

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -1,7 +1,7 @@
 <?php $hero_image=get_field( "hero_image" ); ?>
 <?php $hero_title=get_field( "hero_title" ); ?>
 <?php $hero_sub_title=get_field( "hero_sub_title" ); ?>
-<div class="hero-section">
+<div class="hero-section lines-fullwidth">
 
   <div class="black-plate"></div>
 
@@ -10,14 +10,47 @@
     <div class="hero">
           
       <div class="hero-title-text">
-        <h2 class="h2 hero-title"><?php echo $hero_sub_title ?></h2>
-        <h1 class="h1 hero-sub-title"><?php echo $hero_title ?></h1>
+        <div class="hero-sub-title-wrapper">
+          <div class="orange-vertical-line"></div>
+          <h2 class="h2 hero-sub-title"><?php echo $hero_sub_title ?></h2>
+        </div>
+        <h1 class="h1 hero-title"><?php echo $hero_title ?></h1>
       </div>
 
-      <div class="hero-image">
-        <img src="<?php echo $hero_image ?>" class="header-logotip" alt="hero image">
+      <div class="hero-image-wrapper">
+        <img src="<?php echo $hero_image ?>" class="hero-image" alt="hero image">
       </div>
 
+      <div class="square-dots">
+        <div class="square-dots-row">
+          <div class="square-dot"></div>
+          <div class="square-dot"></div>
+          <div class="square-dot orange-dot"></div>
+          <div class="square-dot green-dot"></div>
+        </div>
+
+        <div class="square-dots-row">
+          <div class="square-dot"></div>
+          <div class="square-dot"></div>
+          <div class="square-dot green-dot"></div>
+          <div class="square-dot green-dot"></div>
+        </div>
+
+        <div class="square-dots-row">
+          <div class="square-dot orange-dot"></div>
+          <div class="square-dot grey-dot"></div>
+          <div class="square-dot orange-dot"></div>
+          <div class="square-dot green-dot"></div>
+        </div>
+
+        <div class="square-dots-row">
+          <div class="square-dot black-dot"></div>
+          <div class="square-dot grey-dot"></div>
+          <div class="square-dot green-dot"></div>
+          <div class="square-dot orange-dot"></div>
+        </div>
+        
+      </div>
 
     </div>
 
